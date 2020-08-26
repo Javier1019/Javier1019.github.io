6f28260289fea1a9ae2f0e2e066b7907953b04d2
Javier Nazario <javand1019@gmail.com>
2020-08-26 02:21:23 -0400

    First Webpage
    React Page.

---
+++ build/precache-manifest.0ae8f1919a8bd2d5ba7a7573c8416892.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbac85c1bc4f1a0f63f09b65c6d7a57b",
+    "url": "/Javier1019/Javier1019.github.io.git/index.html"
+  },
+  {
+    "revision": "a9dff68f4f816eec8c75",
+    "url": "/Javier1019/Javier1019.github.io.git/static/css/main.83546fb7.chunk.css"
+  },
+  {
+    "revision": "72c77b249d97302dc804",
+    "url": "/Javier1019/Javier1019.github.io.git/static/js/2.e1812418.chunk.js"
+  },
+  {
+    "revision": "8cacca417a0415a57132ff77a247cfdb",
+    "url": "/Javier1019/Javier1019.github.io.git/static/js/2.e1812418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9dff68f4f816eec8c75",
+    "url": "/Javier1019/Javier1019.github.io.git/static/js/main.41e5e1a6.chunk.js"
+  },
+  {
+    "revision": "6b0b17a52d341a2b5ab8",
+    "url": "/Javier1019/Javier1019.github.io.git/static/js/runtime-main.0c0ff755.js"
+  },
+  {
+    "revision": "9115dfece0d7fa282f71425fe29053fc",
+    "url": "/Javier1019/Javier1019.github.io.git/static/media/Javier.9115dfec.png"
+  },
+  {
+    "revision": "e9b87d74ea39feb66d8593515f8352d1",
+    "url": "/Javier1019/Javier1019.github.io.git/static/media/JavierNazarioFelicianoResume2020.e9b87d74.pdf"
+  },
+  {
+    "revision": "d8e07c80b386c89a0125dc387b45eec5",
+    "url": "/Javier1019/Javier1019.github.io.git/static/media/LinkedIn-Logo.d8e07c80.png"
+  },
+  {
+    "revision": "23129f8633ec12d76ee0dff5596da3ec",
+    "url": "/Javier1019/Javier1019.github.io.git/static/media/Resume-Icon.23129f86.png"
+  },
+  {
+    "revision": "a3868c8e5de57a23b98eb0c56b1ffb46",
+    "url": "/Javier1019/Javier1019.github.io.git/static/media/Twitter-Logo.a3868c8e.png"
+  }
+]);
